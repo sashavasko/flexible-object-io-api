d0c3ab58b678c95d41562e812c9a1422482d78f8
sashavasko <sashavasko@carfax.com>
2021-01-28 10:37:33 -0600

    Added simple implementation of ClusterResponse

--- flexible-object-io-core/src/main/java/org/sv/flexobject/clustering/ClusterResponse.java
+++ flexible-object-io-core/src/main/java/org/sv/flexobject/clustering/ClusterResponse.java
@@ -2,8 +2,42 @@ package org.sv.flexobject.clustering;
 
 public interface ClusterResponse {
 
-
     boolean isError();
 
     Exception getException();
+
+    class SuccessOrFailure implements ClusterResponse{
+
+        Exception exception;
+
+        public SuccessOrFailure() {
+            this.exception = null;
+        }
+
+        public SuccessOrFailure(Exception exception) {
+            this.exception = exception;
+        }
+
+        @Override
+        public boolean isError() {
+            return exception != null;
+        }
+
+        @Override
+        public Exception getException() {
+            return exception;
+        }
+    }
+
+    static ClusterResponse success(){
+        return new SuccessOrFailure();
+    }
+
+    static ClusterResponse failure(Exception e){
+        return new SuccessOrFailure(e);
+    }
+
+    static ClusterResponse failure(String error){
+        return new SuccessOrFailure(new RuntimeException(error));
+    }
 }
