d16718855c9d72612633d97b53c7c2f056f96f24
sashavasko <sashavasko@carfax.com>
2021-03-05 12:32:24 -0600

    Fixed Untit tests

--- flexible-object-io-hadoop/src/test/java/org/sv/flexobject/hadoop/HadoopTaskConfTest.java
+++ flexible-object-io-hadoop/src/test/java/org/sv/flexobject/hadoop/HadoopTaskConfTest.java
@@ -1,11 +1,11 @@
 package org.sv.flexobject.hadoop;
 
-import com.carfax.dt.streaming.schema.Schema;
-import com.carfax.dt.streaming.schema.SchemaElement;
-import com.carfax.hadoop.properties.HadoopPropertiesProvider;
-import com.carfax.hadoop.properties.HadoopSecretProvider;
 import org.apache.hadoop.conf.Configuration;
 import org.junit.Test;
+import org.sv.flexobject.hadoop.properties.HadoopPropertiesProvider;
+import org.sv.flexobject.hadoop.properties.HadoopSecretProvider;
+import org.sv.flexobject.schema.Schema;
+import org.sv.flexobject.schema.SchemaElement;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,10 +19,10 @@ public class HadoopTaskConfTest {
     public void listSettings() {
         HadoopTaskConf conf = new HadoopTaskConf();
         List<String> expectedSettings = Arrays.asList(
-                "cfx.hadoop.db.environment",
-                "cfx.hadoop.deployment.level",
-                "cfx.hadoop.connection.manager.providers",
-                "cfx.hadoop.connection.manager.environment");
+                "org.sv.flexobject.hadoop.db.environment",
+                "org.sv.flexobject.hadoop.deployment.level",
+                "org.sv.flexobject.hadoop.connection.manager.providers",
+                "org.sv.flexobject.hadoop.connection.manager.environment");
         List<String> actualSettings = new ArrayList<>();
 
         for (SchemaElement e : Schema.getRegisteredSchema(conf.getClass()).getFields()){
@@ -35,7 +35,7 @@ public class HadoopTaskConfTest {
     @Test
     public void providerClasses() {
         Configuration configuration = new Configuration(false);
-        configuration.set("cfx.hadoop.connection.manager.providers", HadoopSecretProvider.class.getName() + "," + HadoopPropertiesProvider.class.getName());
+        configuration.set("org.sv.flexobject.hadoop.connection.manager.providers", HadoopSecretProvider.class.getName() + "," + HadoopPropertiesProvider.class.getName());
         HadoopTaskConf conf = new HadoopTaskConf();
 
         conf.from(configuration);
