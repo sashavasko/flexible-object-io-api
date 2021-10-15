60c962184eba52f013765aaf8d27e178d9e69749
sashavasko <sashavasko@carfax.com>
2021-10-15 11:22:45 -0500

    Added composition to FunctionWithException Made TransformSink use lambda for transformation allowing for customization Changed TransformSink to stop on null data

--- flexible-object-io-core/src/main/java/org/sv/flexobject/stream/sinks/TransformSink.java
+++ flexible-object-io-core/src/main/java/org/sv/flexobject/stream/sinks/TransformSink.java
@@ -3,10 +3,12 @@ package org.sv.flexobject.stream.sinks;
 
 import org.sv.flexobject.stream.Sink;
 import org.sv.flexobject.stream.Source;
+import org.sv.flexobject.util.FunctionWithException;
 
-public abstract class TransformSink<SELF,INPUT,OUTPUT> implements Sink<INPUT> {
+public class TransformSink<SELF,INPUT,OUTPUT> implements Sink<INPUT> {
 
     Sink<OUTPUT> outputSink;
+    FunctionWithException<INPUT, OUTPUT, Exception> transformer = (input)->(OUTPUT)input;
 
     public TransformSink() {
     }
@@ -19,18 +21,30 @@ public abstract class TransformSink<SELF,INPUT,OUTPUT> implements Sink<INPUT> {
         this.outputSink = outputSink;
         return (SELF) this;
     }
+    public SELF setTransform(FunctionWithException<INPUT, OUTPUT, Exception> transformer) {
+        this.transformer = transformer;
+        return (SELF) this;
+    }
 
-    abstract public OUTPUT transform(INPUT input);
+    public OUTPUT transform(INPUT input) throws Exception{
+        return transformer.apply(input);
+    }
 
     public void transformAll(Source<INPUT> source) throws Exception {
-        while (!source.isEOF())
-            put(source.get());
+        INPUT data;
+        while ((data = source.get()) != null) {
+            if (!put(data))
+                break;
+        }
         setEOF();
     }
 
     @Override
     public boolean put(INPUT value) throws Exception {
-        return outputSink.put(transform(value));
+        OUTPUT output = transform(value);
+        if (output == null)
+            return false;
+        return outputSink.put(output);
     }
 
     @Override
