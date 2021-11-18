da8375ef082cd2f0db97b34b4c80f1a2fef38615
sashavasko <sashavasko@carfax.com>
2021-11-18 11:51:41 -0600

    Added docs for SparkMain

--- flexible-object-io-hadoop/src/main/java/org/sv/flexobject/hadoop/SparkMain.java
+++ flexible-object-io-hadoop/src/main/java/org/sv/flexobject/hadoop/SparkMain.java
@@ -4,6 +4,59 @@ import org.apache.spark.sql.SparkSession;
 
 public class SparkMain {
 
+    /**
+     * The entry point into Spark application. This will be run by Application Master.
+     * By default it will instantiate Spark session based on spark.* options supplied in
+     * configuration file via -conf command line option. This configuration is bundled
+     * with jar file and uploaded to the cluster by Spark Client when run via hdpspark command.
+     *
+     * main() method will attempt to instantiate user defined class:
+     *
+     * {{{
+     * public class MySparkRunner extends SparkTool{
+     *      int run(String[] var1) throws Exception{
+     *
+     *          // your workflow goes here
+     *
+     *          return 0;
+     *      }
+     * }
+     *}}}
+     *
+     * that class should be specified in spark configuration like so:
+     * {{{
+     *     -Dspark.hadoop.tool.class=my.package.MySparkRunner
+     * }}}
+     *
+     * or via config file like so :
+     * {{{
+     *      <property><name>spark.hadoop.tool.class</name><value>my.package.MySparkRunner</value></property>
+     * }}}
+     *
+     * For unit testing the following steps should be followed :
+     *
+     * Setup unit tests by creating local SparkSession like so :
+     * (optionally adding some of the desired configuration parameters)
+     *
+     * {{{
+     *   SparkSession spark = SparkSession.builder()
+     *      .master("local")
+     *      .config("option", "value")
+     *      .getOrCreate()
+     * }}}
+     *
+     * If you need to work with Connection Providers - configure the HadoopTask like so :
+     * {{{
+     *      HadoopTask.configure(spark.sparkContext());
+     * }}}
+     *
+     * If you are testing the SparkTool implementation - make sure you supply spark session to it like so :
+     * {{{
+     *     tool.setSession(spark);
+     * }}}
+     *
+     */
+
     public static void main(String[] args){
         SparkSession spark = SparkSession.builder()
                 .getOrCreate();
