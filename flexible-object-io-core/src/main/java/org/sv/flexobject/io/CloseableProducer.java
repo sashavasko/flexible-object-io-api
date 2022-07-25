fdcefac3c9b77fffcc8a55d8b5d4d8d6f12dd184
sashavasko <sashavasko@carfax.com>
2022-07-25 13:08:17 -0500

    Added CloseableProducer.Error

--- flexible-object-io-core/src/main/java/org/sv/flexobject/io/CloseableProducer.java
+++ flexible-object-io-core/src/main/java/org/sv/flexobject/io/CloseableProducer.java
@@ -2,6 +2,8 @@ package org.sv.flexobject.io;
 
 import org.sv.flexobject.Loadable;
 
+import java.util.Iterator;
+
 public abstract class CloseableProducer<T extends Loadable> extends Producer<T> implements  AutoCloseable {
 
     public abstract void unsafeClose() throws Exception;
@@ -13,6 +15,36 @@ public abstract class CloseableProducer<T extends Loadable> extends Producer<T> 
         }catch (Exception e){
             if (getException() == null)
                 setException(e);
+        }
+    }
+    public static class Error extends CloseableProducer {
+
+        public Error(Throwable e) {
+            setException(e);
+        }
+
+        @Override
+        public Iterator iterator() {
+            return new Iterator() {
+                @Override
+                public boolean hasNext() {
+                    return false;
+                }
+
+                @Override
+                public Object next() {
+                    return null;
+                }
+            };
+        }
+
+        @Override
+        public Loadable produce() {
+            return null;
+        }
+
+        @Override
+        public void unsafeClose() throws Exception {
         }
     }
 }
