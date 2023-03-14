dac7aaffb180d703f4975db679ec86c4c46757ae
sashavasko <sashavasko@carfax.com>
2023-03-14 15:25:35 -0500

    added put() method for the Iterator

--- flexible-object-io-core/src/main/java/org/sv/flexobject/stream/Sink.java
+++ flexible-object-io-core/src/main/java/org/sv/flexobject/stream/Sink.java
@@ -9,6 +9,19 @@ public interface Sink<T> {
      */
     boolean put(T value) throws Exception;
 
+    /**
+     * @param values
+     * @return If sink is full returns true, otherwise false;
+     * @throws Exception
+     */
+    default boolean put(Iterator<T> values) throws Exception{
+        while (values.hasNext()) {
+            if (put(values.next()))
+                return true;
+        }
+        return false;
+    }
+
     default void setEOF() {}
 
     boolean hasOutput();
