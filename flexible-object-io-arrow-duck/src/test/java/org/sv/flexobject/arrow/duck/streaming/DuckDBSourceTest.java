8980f363de1f1afe98bc6df7e17ea3d0e5d292f5
sashavasko <sashavasko@gmail.com>
2026-07-22 15:45:23 -0500

    Remove useless annoying warnings

--- flexible-object-io-arrow-duck/src/test/java/org/sv/flexobject/arrow/duck/streaming/DuckDBSourceTest.java
+++ flexible-object-io-arrow-duck/src/test/java/org/sv/flexobject/arrow/duck/streaming/DuckDBSourceTest.java
@@ -1,20 +1,13 @@
 package org.sv.flexobject.arrow.duck.streaming;
 
-import org.apache.arrow.memory.RootAllocator;
-import org.apache.arrow.vector.ipc.ArrowReader;
-import org.duckdb.DuckDBConnection;
-import org.duckdb.DuckDBResultSet;
 import org.junit.jupiter.api.Test;
 import org.sv.flexobject.Streamable;
-import org.sv.flexobject.arrow.read.ArrowRootReader;
 import org.sv.flexobject.connections.ConnectionManager;
 import org.sv.flexobject.properties.FilePropertiesProvider;
 import org.sv.flexobject.testdata.TestDataWithSubSchemaInCollection;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class DuckDBSourceTest {
 
